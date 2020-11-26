a294a74103aaa9e543f0cd99e2b01064b73cd087
Evgenia Kurineva <kurineva85@mail.ru>
2020-11-26 13:02:45 +0300

    Version 21.0: added user status, using local state and setState() for edit mode user status on page Profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import Preloader from "../../common/preloader/Preloader";
 import s from "./ProfileInfo.module.css";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
-  if(!props.profile) return <Preloader/>
-   return (
-       <div className={s.info}>
-         <div className={s.infoImg}>
-           <img
+  if (!props.profile) return <Preloader />;
+  return (
+    <div>
+      <div className={s.info}>
+        <div className={s.infoImg}>
+          <img
             //  src="https://hypeava.ru/uploads/posts/2018-03/1521909692_4.jpg"
-            src={props.profile.photos.large?props.profile.photos.large:"https://hypeava.ru/uploads/posts/2018-03/1521909692_4.jpg"}
-             alt=""
-           />
-         </div>
-         <div className={s.infoText}>{props.profile.fullName}</div>
-       </div>
-   );
- };
+            src={
+              props.profile.photos.large
+                ? props.profile.photos.large
+                : "https://hypeava.ru/uploads/posts/2018-03/1521909692_4.jpg"
+            }
+            alt=""
+          />
+        </div>
+        <div className={s.infoText}>{props.profile.fullName}</div>
+      </div>
+      <ProfileStatus status="Hello, my friends!" />
+    </div>
+  );
+};
 
- export default ProfileInfo;
+export default ProfileInfo;
