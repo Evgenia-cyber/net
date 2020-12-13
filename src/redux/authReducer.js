31de7041ec104c5f0cfea81cf20909212c7ff62b
Evgenia Kurineva <kurineva85@mail.ru>
2020-12-13 11:42:18 +0300

    Version 33.0: refactoring: used redux-ducks agreement and async await instead then to refactoring reducers

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,7 +1,7 @@
 import { stopSubmit } from "redux-form";
 import { authAPI } from "../api/api";
 
-const SET_AUTH_USER_DATA = "SET_AUTH_USER_DATA";
+const SET_AUTH_USER_DATA = "my-app/auth/SET_AUTH_USER_DATA";
 
 let initialState = {
   id: null,
@@ -27,17 +27,16 @@ export const setAuthUserData = (id, email, login, isAuth) => ({
   data: { id, email, login, isAuth },
 });
 
-export const getAuthUserData = () => (dispatch) => {
-  return authAPI.authMe().then((response) => {
+export const getAuthUserData = () =>async (dispatch) => {
+   let response = await authAPI.authMe();
     if (response.data.resultCode === 0) {
       let { id, email, login } = response.data.data; //деструктуризация - получаем из объекта его свойства в одноименные переменные
       dispatch(setAuthUserData(id, email, login, true));
     }
-  });
 };
 
-export const login = (email, password, rememberMe) => (dispatch) => {
-  authAPI.login(email, password, rememberMe).then((response) => {
+export const login = (email, password, rememberMe) => async (dispatch) => {
+  let response = await authAPI.login(email, password, rememberMe);
     if (response.data.resultCode === 0) {
       dispatch(getAuthUserData());
     } else {
@@ -46,15 +45,13 @@ export const login = (email, password, rememberMe) => (dispatch) => {
       const action = stopSubmit("login", { _error: errorMessage });
       dispatch(action);
     }
-  });
 };
 
-export const logout = () => (dispatch) => {
-  authAPI.logout().then((response) => {
+export const logout = () => async (dispatch) => {
+  let response = await authAPI.logout();
     if (response.data.resultCode === 0) {
       dispatch(setAuthUserData(null, null, null, false));
     }
-  });
 };
 
 export default authReducer;
